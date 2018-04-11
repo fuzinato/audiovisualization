6680c46195ef426aca989a8d126a638834f18254
Dario Fuzinato <fussinatto@gmail.com>
2018-04-11 22:03:49 +0200

    style No.1

--- src/audiovisualisierung.js
+++ src/audiovisualisierung.js
@@ -43,16 +43,11 @@ function handleFiles(files) {
 	var fileReader  = new FileReader();
     fileReader.onload = function(){
          var arrayBuffer = this.result;
-         console.log(arrayBuffer);
-         console.log(arrayBuffer.byteLength);
 		
 		 filename = files[0].name.toString();
 		filename = filename.slice(0, -4);
-		console.log(filename);
 
 		var url = files[0].urn || files[0].name;
-		ID3.loadTags(url, function() {
-			var tags = ID3.getAllTags(url);
 
 //                    console.log(tags.title.toString().length);
 //                    if (tags.title.length > 14) {
@@ -63,36 +58,11 @@ function handleFiles(files) {
 //                    else {
 //                        $("#title").html(tags.title);
 //                    }
-			if (tags.title.length > 14 && tags.title.length <= 17) {
 
-				$("#title").css("font-size", "7.5vh");
-				
-			}
-			if (tags.title.length > 17 && tags.title.length <= 20) {
-				
-				$("#title").css("font-size", "6.5vh");
-			}
-			
-			if (tags.title.length > 20) {
-				
-				$("#title").css("font-size", "5vh");
-				
-			}
-			
-			$("#title").html(tags.title);
 			
 			onWindowResize();
 
-			$("#title").css("visibility", "visible");
-
-			$("#artist").html(tags.artist);
-			$("#artist").css("visibility", "visible");
-			$("#album").html(tags.album);
-			$("#album").css("visibility", "visible");
-		  }, {
-			tags: ["title","artist","album","picture"],
-			dataReader: ID3.FileAPIReader(files[0])
-		  });
+			
 		
      };
      fileReader.readAsArrayBuffer(files[0]);
@@ -144,18 +114,14 @@ function playSample() {
  	// When loaded decode the data
 	request.onload = function() {
 		
-		$("#title").html("Infinite");
-		$("#album").html("Infinite");
-		$("#artist").html("Valence");
+	
 		onWindowResize();
-		$("#title, #artist, #album").css("visibility", "visible");
 		
 		// decode the data
 		context.decodeAudioData(request.response, function(buffer) {
 		// when the audio is decoded play the sound
 		sourceNode.buffer = buffer;
 		sourceNode.start(0);
-		$("#freq, body").addClass("animateHue");
 		//on error
 		}, function(e) {
 			console.log(e);
@@ -164,42 +130,6 @@ function playSample() {
 	request.send();
 	
 	$("button, input").prop("disabled",true);
-}
-
-function useMic() 	
-{
-	"use strict";
-	if (!navigator.mediaDevices.getUserMedia) {
-		alert("Your browser does not support microphone input!");
-		console.log('Your browser does not support microphone input!');
-		return;
- 	}
-	
-	navigator.mediaDevices.getUserMedia({audio: true, video: false})
-	.then(function(stream) {
-		hasSetupUserMedia = true;
-	  	//convert audio stream to mediaStreamSource (node)
-		microphone = context.createMediaStreamSource(stream);
-		//create analyser
-		if (analyser === null) analyser = context.createAnalyser();
-		//connect microphone to analyser
-		microphone.connect(analyser);
-		//start updating
-		rafID = window.requestAnimationFrame( updateVisualization );
-		
-		$("#title").html("Mic");
-		$("#album").html("Input");
-		$("#artist").html("Using");
-		onWindowResize();
-		$("#title, #artist, #album").css("visibility", "visible");
-		$("#freq, body").addClass("animateHue");
-	})
-	.catch(function(err) {
-	  /* handle the error */
-		alert("Capturing microphone data failed! (currently only supported in Chrome & Firefox)");
-		console.log('capturing microphone data failed!');
-		console.log(err);
-	});
 }
 
 // progress on transfers from the server to the client (downloads)
@@ -238,7 +168,7 @@ function initBinCanvas () {
 	"use strict";
 	c = document.getElementById("freq");
 	c.width = window.innerWidth;
-        c.height = window.innerHeight;
+  c.height = window.innerHeight;
 	//get context from canvas for drawing
 	ctx = c.getContext("2d");
 	
@@ -247,13 +177,6 @@ function initBinCanvas () {
 	
 	window.addEventListener( 'resize', onWindowResize, false );
 	
-	//create gradient for the bins
-	var gradient = ctx.createLinearGradient(0, c.height - 300,0,window.innerHeight - 25);
-	gradient.addColorStop(1,'#00f'); //black
-	gradient.addColorStop(0.75,'#f00'); //red
-	gradient.addColorStop(0.25,'#f00'); //yellow
-	gradient.addColorStop(0,'#ffff00'); //white
-
 	
 	ctx.fillStyle = "#9c0001";
 }
@@ -261,17 +184,8 @@ function initBinCanvas () {
 function onWindowResize()
 {
 	ctx.canvas.width  = window.innerWidth;
-  	ctx.canvas.height = window.innerHeight;
-	
-	var containerHeight = $("#song_info_wrapper").height();
-	var topVal = $(window).height() / 2 - containerHeight / 2; 
-	$("#song_info_wrapper").css("top", topVal);
-	console.log(topVal);
-	
-	if($(window).width() <= 500) {
-		//TODO: not yet working
-		$("#title").css("font-size", "40px");
-	}
+	ctx.canvas.height = window.innerHeight;
+
 }
 
 var audioBuffer;
@@ -279,6 +193,7 @@ var sourceNode;
 function setupAudioNodes() {
 	// setup a analyser
 	analyser = context.createAnalyser();
+	// analyser.fftSize = 512;
 	// create a buffer source node
 	sourceNode = context.createBufferSource();	
 	//connect source to analyser as link
@@ -329,64 +244,58 @@ function drawBars (array) {
 	ctx.save();
 
 
-	ctx.globalCompositeOperation='source-over';
+	// ctx.globalCompositeOperation='source-over';
 
 	//console.log(maxBinCount); //--> 1024
-	ctx.scale(0.5, 0.5);
-	ctx.translate(window.innerWidth, window.innerHeight);
+	// ctx.scale(0.5, 0.5);
+	ctx.translate(window.innerWidth/2, window.innerHeight/2);
 	ctx.fillStyle = "#fff";
 
 	var bass = Math.floor(array[1]); //1Hz Frequenz 
-	var radius = 0.45 * $(window).width() <= 450 ? -(bass * 0.25 + 0.45 * $(window).width()) : -(bass * 0.25 + 450);
-
+	var radius = -100;//0.45 * $(window).width() <= 450 ? -(bass * 0.25 + 0.45 * $(window).width()) : -(bass * 0.25 + 450);
 	var bar_length_factor = 1;
-	if ($(window).width() >= 785) {
-		bar_length_factor = 1.0;
-	}
-	else if ($(window).width() < 785) {
-		bar_length_factor = 1.5;
-	}
-	else if ($(window).width() < 500) {
-		bar_length_factor = 20.0;
-	}
-	console.log($(window).width());
+	// if ($(window).width() >= 785) {
+	// }
+	// else if ($(window).width() < 785) {
+	// 	bar_length_factor = 1.5;
+	// }
+	// else if ($(window).width() < 500) {
+	// 	bar_length_factor = 20.0;
+	// }
 	//go over each bin
-	for ( var i = 0; i < maxBinCount; i++ ){
+
+
+	// for ( var i = 0; i < maxBinCount; i++ ){
 		
-		var value = array[i];
-		if (value >= threshold) {			
+	// 	var value = array[i];
+	// 	if (value >= threshold) {			
 			//draw bin
 			//ctx.fillRect(0 + i * space, c.height - value, 2 , c.height);
                         //ctx.fillRect(i * space, c.height, 2, -value);
-                        ctx.fillRect(0, radius, $(window).width() <= 450 ? 2 : 3, -value / bar_length_factor);
-                        ctx.rotate((180 / 128) * Math.PI/180);   
-		}
-	}  
-        
+                        // ctx.fillRect(0, radius, $(window).width() <= 450 ? 2 : 3, -value / bar_length_factor);
+                        // ctx.rotate((180 / 128) * Math.PI/180);   
+	// 	}
+	// }  
+
+	
 	for ( var i = 0; i < maxBinCount; i++ ){
 
 		var value = array[i];
+		const refAngle = 360 / 456;
 		if (value >= threshold) {				
+			const refVal = array[201]
+			if (i < 200) {
+				value *= i*0.5/200 + 0.5
+      }
+			
+			//   ctx.rotate(-(180 / 456) * Math.PI / 180);
+			// ctx.fillRect(0, radius, 2, -value / bar_length_factor);
 
-			//draw bin
-			//ctx.fillRect(0 + i * space, c.height - value, 2 , c.height);
-						//ctx.fillRect(i * space, c.height, 2, -value);
-						ctx.rotate(-(180 / 128) * Math.PI/180);
-						ctx.fillRect(0, radius, $(window).width() <= 450 ? 2 : 3, -value / bar_length_factor);
+			const x = Math.sin(refAngle * i) * value;
+			const y = Math.cos(refAngle * i) * value;
+			ctx.fillRect(x, y, 2, 2);
 		}
-	} 
-        
-	for ( var i = 0; i < maxBinCount; i++ ){
 
-		var value = array[i];
-		if (value >= threshold) {				
-
-			//draw bin
-			//ctx.fillRect(0 + i * space, c.height - value, 2 , c.height);
-						//ctx.fillRect(i * space, c.height, 2, -value);
-						ctx.rotate((180 / 128) * Math.PI/180);
-						ctx.fillRect(0, radius, $(window).width() <= 450 ? 2 : 3, -value / bar_length_factor);
-		}
 	} 
     
 	ctx.restore();
